628760ea82d819c0ac670e9aed1665106fec62d1
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-12-04 10:54:03 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+
+character_name = "Tom"
+character_age = 50.3
+
+print("There once was a man named " + character_name + ", ")
+print("He was " + str(character_age) + " years old. ")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ", ")
+print("but did't like being " + str(character_age) + ".")
+
