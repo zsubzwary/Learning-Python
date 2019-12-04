6e3a01ad2b7691f521baa3193c64b6f88324adef
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-12-04 11:41:24 +0500

    more math functions
    Signed-off-by: Zakawat <31027252+zsubzwary@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
-print(12345)
-print(3.14)
-print(-35)
-print(3 + 4.5)
-print(3 - 4.5)
-print(3 * 4.5)
-print(3 / 4.5)
-print(3 % 4.5)
-
-print(3 * 4 + 5)
-print(3 * (4 + 5))
-
-my_num = 5
-print(str(my_num) + " is my fav number")
+from math import *
 
 my_num = -5
-print(abs(my_num))
-
-print(pow(4, 6))
-print(min(4, 6))
-print(max(4, 6))
-print(round(3.5))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(3.7))
