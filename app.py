7969ece10eef99fe5f4720b718f2aecdd0fa7b19
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-12-04 11:38:17 +0500

    numbers
    Signed-off-by: Zakawat <31027252+zsubzwary@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-phrase = "abc"
-print(phrase.upper())
-print(phrase.lower())
+print(12345)
+print(3.14)
+print(-35)
+print(3 + 4.5)
+print(3 - 4.5)
+print(3 * 4.5)
+print(3 / 4.5)
+print(3 % 4.5)
 
-print(len(phrase))
-print(phrase.isupper())
-print(phrase.upper().isupper())
+print(3 * 4 + 5)
+print(3 * (4 + 5))
 
-print(phrase[0])
-print(phrase.index("c"))
-print(phrase.replace("a", "1"))
+my_num = 5
+print(str(my_num) + " is my fav number")
+
+my_num = -5
+print(abs(my_num))
+
+print(pow(4, 6))
+print(min(4, 6))
+print(max(4, 6))
+print(round(3.5))
